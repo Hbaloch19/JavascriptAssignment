df7d220e77e13b3c74870f5f4ab0bcfc9ad235d2
Hbaloch19 <167871824+Hbaloch19@users.noreply.github.com>
2024-10-28 21:43:48 +0500

    Add files via upload

---
+++ Assigment JS/app.js
@@ -0,0 +1,67 @@
+//ASSIGNMENT # 02
+//QNO1
+let username;
+//QNO2
+let myName = "Hanadi Baloch";
+//QNO3
+let message = "Hello World!";
+alert(message);
+//QNO4
+let stdname = "Hanadi";
+let std_Fname = "M.Rafiq";
+let courseName = "web & mobile app development";
+alert(stdname);
+alert(std_Fname);
+alert(courseName);
+//QNO5
+let word = "PIZZA";
+let result = "";
+for (let i = word.length; i > 0; i--) {
+  result += word.slice(0, i) + "\n";
+}
+alert(result);
+//QNO6
+let email ="hanadirafiq1927@gamil.com";
+alert(email);
+//QNO7
+let book = "A smarter way to learn JavaScript";
+document.write(book);
+//QNO8
+let variable = "Yeah ! I can write html contents through js.";
+document.write(variable); 
+//QNO9
+let variable1 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(variable1);
+
+//END!
+
+//ASSIGNMENT # 03
+//QNO01
+let age = "I'm 20 years old";
+alert(age);
+//QNO3
+let birthYear = "My Birthyear is 2002 <br> Data type of declared variable is number";
+document.write(birthYear);
+//QNO4
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+var messagee = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+document.write(messagee);
+//ASSIGNMENT # 04
+//QNO1
+let firstName = "Hanadi", lastName = "Baloch", Age = 20;
+//QNO2
+//LEGAL VARIABLE NAME:
+Hanadi;
+_myhanadi;
+$myhanadi;
+hanadi1;
+hanadi123;
+//ILLLEGAL VARIABLE NAME:
+1sthanadi;
+my-variable;
+ha nadi;
+@hanadi;
+let;
+   //END!
